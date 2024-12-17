086278faa18158a563065ceedf2686a3f37809a4
Mayankyadav3980 <ymayank501@gmail.com>
2024-12-17 12:38:29 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      be calm, app is up and running
+    </div>
+  );
+}
+
+export default App;
