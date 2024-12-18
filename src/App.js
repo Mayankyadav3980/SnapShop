da49f3eb5eb7ade19a933a20f3dd42512ee1a508
Mayankyadav3980 <ymayank501@gmail.com>
2024-12-18 13:10:47 +0530

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import "./App.css";
+
 import Nav from "./components/Nav/Nav";
 import SignUp from "./pages/SignUp/SignUp";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import "./App.css";
 
 function App() {
   const router = createBrowserRouter([
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,14 +1,33 @@
-import React from "react";
-import styles from "./SignUp.module.css";
+import React, { useState, useRef } from "react";
+// import styles from "./SignUp.module.css";
 
 const SignUp = () => {
+  const [signUpDetail, setSignUpDetail] = useState({
+    email:'',
+    password:''
+  })
+
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(emailRef.current.value, passwordRef.current.value);
+    
+    setSignUpDetail({
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    });
+  }
   return (
-    <div className={styles.form_wrapper}>
-      <form className={styles.form_container}>
-        <h2 className={styles.heading}>SignUp Form</h2>
-        <input placeholder="email" />
-        <input placeholder="password" />
-        <button type="submit">Submit</button>
+    <div className="form_wrapper">
+      <form className="form_container" onSubmit={handleSubmit}>
+        <h2 className="heading">SignUp Form</h2>
+        <input type='email' placeholder="email" name="email" id="email" ref={emailRef} required />
+        <input type="password" placeholder="password" name="password" id="password" ref={passwordRef} required />
+        <button type="submit" className="btn btn-outline-secondary">
+          Submit
+        </button>
       </form>
     </div>
   );
